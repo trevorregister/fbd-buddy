8759e2107f79d34369815b93459d6467be144ec9
trevorregister <trevor.register@gmail.com>
2024-09-06 18:07:21 -0400

    feat: draw arrow between clicks

--- app.vue
+++ app.vue
@@ -3,15 +3,19 @@
     <v-main>
       <v-container>
         <ClientOnly>
-          <v-stage :config="configKonva" @dblclick="drawCircle"> 
+          <v-stage 
+            :config="configKonva" 
+            @dblclick="drawCircle"
+            @click="drawArrow"
+          > 
             <v-layer>
               <v-circle v-for="circle in circles" :config="circle" />
-              <v-arrow :config="configArrow" />
+              <v-arrow v-for="arrow in arrows" :config="arrow" />
             </v-layer>
           </v-stage>
         </ClientOnly>
-        <v-btn @click="clearCircles">
-            Clear Circles
+        <v-btn @click="clearCanvas">
+            Clear Canvas
         </v-btn>
       </v-container>
     </v-main>
@@ -22,26 +26,13 @@
 import { ref } from 'vue'
 const clickCoords = ref({x: 0, y: 0})
 const circles = ref([])
+const arrows = ref([])
+const arrowPointsBeingDrawn = ref([])
+const isDrawingArrow = ref(false)
 
 const configKonva = {
   width: 500,
   height: 500,
-}
-
-const configCircle = {
-  x: 100,
-  y: 100,
-  radius: 5,
-  fill: 'black',
-  stroke: 'black',
-  strokeWidth: 4,
-}
-
-const configArrow = {
-  points: [configCircle.x, configCircle.y, configCircle.x+100, configCircle.y],
-  fill: 'black',
-  stroke: 'black',
-  strokeWidth: 4,
 }
 
 const drawCircle = (e) => {
@@ -61,8 +52,32 @@ const drawCircle = (e) => {
   return circle
 }
 
-const clearCircles = () => {
+const drawArrow = (e) => {
+  const arrow = {  
+    fill: 'black',
+    stroke: 'black',
+    strokeWidth: 4,
+    points: []
+  }
+  const stage = e.target.getStage()
+  const pointerPosition = stage.getPointerPosition()
+  clickCoords.value = pointerPosition
+
+  if (!isDrawingArrow.value) {
+    arrowPointsBeingDrawn.value = [pointerPosition.x, pointerPosition.y]
+    isDrawingArrow.value = true
+  } else {
+    arrow.points = [...arrowPointsBeingDrawn.value, pointerPosition.x, pointerPosition.y]
+    arrows.value.push(arrow)
+    arrowPointsBeingDrawn.value = []
+    isDrawingArrow.value = false
+  }
+}
+
+const clearCanvas = () => {
   circles.value = []
+  arrows.value = []
+  arrowPointsBeingDrawn.value = []
 }
   
 </script>
