60267944dd3a409de376a984bfc399d559515861
trevorregister <trevor.register@gmail.com>
2024-09-06 16:44:45 -0400

    feat: draw circle at click

--- app.vue
+++ app.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
     <ClientOnly>
-      <v-stage :config="configKonva"> 
+      <v-stage :config="configKonva" @click="drawCircle"> 
         <v-layer>
-          <v-circle :config="configCircle" />
+          <v-circle v-for="circle in circles" :config="circle" />
+          <v-arrow :config="configArrow" />
         </v-layer>
       </v-stage>
+      <button @click="clearCircles">
+          Clear Circles
+        </button>
     </ClientOnly>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
+const clickCoords = ref({x: 0, y: 0})
+const circles = ref([])
 
 const configKonva = {
   width: 500,
@@ -21,10 +27,38 @@ const configKonva = {
 const configCircle = {
   x: 100,
   y: 100,
-  radius: 50,
-  fill: 'red',
+  radius: 5,
+  fill: 'black',
   stroke: 'black',
   strokeWidth: 4,
-};
+}
+
+const configArrow = {
+  points: [configCircle.x, configCircle.y, configCircle.x+100, configCircle.y],
+  fill: 'black',
+  stroke: 'black',
+  strokeWidth: 4,
+}
+
+const drawCircle = (e) => {
+  const stage = e.target.getStage()
+  const pointerPosition = stage.getPointerPosition()
+  clickCoords.value = pointerPosition
+
+  const circle = {
+    x: pointerPosition.x,
+    y: pointerPosition.y,
+    radius: 5,
+    fill: 'black',
+    stroke: 'black',
+    strokeWidth: 4,
+  }
+  circles.value.push(circle)
+  return circle
+}
+
+const clearCircles = () => {
+  circles.value = []
+}
   
 </script>
