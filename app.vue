15c7cd6c8e79a56752cb5472fa93f24cd4207e1f
trevorregister <trevor.register@gmail.com>
2024-09-08 21:04:50 -0400

    feat: add force and clear vectors buttons
    Co-authored-by: John Burk <john.burk98@gmail.com>

--- app.vue
+++ app.vue
@@ -7,12 +7,19 @@
             <v-layer>
               <Grid :spacing="50" />
               <Point :x="10" :y="20" />
+              <ForceVector v-for="vector in forceVectors" 
+                :tail="vector.tail" 
+                :head="vector.head" 
+              />
               <!-- Add more points and arrows as needed -->
             </v-layer>
           </v-stage>
         </ClientOnly>
-        <v-btn @click="clearCanvas">
-          Clear Canvas
+        <v-btn @click="clearForceVectors">
+          Clear Vectors
+        </v-btn>
+        <v-btn @click="addForceVector">
+          Add Force Vector
         </v-btn>
       </v-container>
     </v-main>
@@ -23,12 +30,27 @@
 import { ref } from 'vue'
 import Grid from '~/components/Grid.vue'
 import Point from '~/components/Point.vue'
+import ForceVector from '~/components/ForceVector.vue'
 import { provideCanvasDimensions } from '~/composables/useCanvasDimensions'
 
 const configStage = {
   width: 500,
   height: 500,
 }
+const forceVectors = ref([])
+
+const addForceVector = () => {
+  // keeping it simple for now
+  forceVectors.value.push({ 
+    tail: { x: 0, y: 0 }, 
+    head: { x: 50, y: 50 } 
+  })
+}
+
+const clearForceVectors = () => {
+  forceVectors.value = []
+}
+
 
 // Provide canvas dimensions to all child components
 provideCanvasDimensions(configStage.width, configStage.height)
